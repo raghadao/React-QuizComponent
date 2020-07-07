dbdc35b870b9f3e9be543065264121018cf34d66
raghadao <67149416+raghadao@users.noreply.github.com>
2020-07-08 00:53:31 +0300

    2

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,9 @@ class Quiz extends Component {
     return (
       <div>
         
-        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+        {isQuizEnd ? <QuizEnd /> :
+         <QuizQuestion quiz_question={quizData.quiz_questions
+         [this.state.quiz_position - 1]} />}
       </div>
     )
   }
