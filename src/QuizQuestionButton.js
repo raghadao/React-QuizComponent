4eaa420b10566a114ddf1e6e9a10b9f0f6ee7374
raghadao <67149416+raghadao@users.noreply.github.com>
2020-07-08 00:04:30 +0300

    1

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 
-class QuizQuestionButton extends Component{
- render(){
-return(
-    <li>
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+      <li>
         <button>{this.props.button_text}</button>
-        
-    </li>
-)
-
- }
-    
-
+      </li>
+    )
+  }
 }
 
-export default QuizQuestionButton
+export default QuizQuestionButton 
