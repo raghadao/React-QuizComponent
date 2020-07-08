0f0ef60c43a58a85f90f2a6795265ea1689f97e7
raghadao <67149416+raghadao@users.noreply.github.com>
2020-07-08 03:14:04 +0300

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
 import QuizEnd from './QuizEnd.js'
+import { stat } from 'fs';
 
 let quizData = require('./quiz_data.json')
 
@@ -14,14 +15,19 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 }
     })
   }
+   handleResetClick(){
+     
+    this.setState({ quiz_position: 1})
+
+   }
   render() {
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
     return (
       <div>
-        
-        {isQuizEnd ? <QuizEnd /> :
-         <QuizQuestion quiz_question={quizData.quiz_questions
-         [this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : 
+        <QuizQuestion quiz_question={quizData.quiz_questions
+        [this.state.quiz_position - 1]} showNextQuestionHandler=
+        {this.showNextQuestion.bind(this)} />}
       </div>
     )
   }
